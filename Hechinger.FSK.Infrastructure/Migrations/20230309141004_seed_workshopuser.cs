43dd89b694417e1e7f7d36f0f06f8bd29f476fd5
holeczpeter <holecz.peter85@gmail.com>
2023-03-09 15:30:11 +0100

    workshopusers migration users.json add new user usermanual copy always

---
+++ Hechinger.FSK.Infrastructure/Migrations/20230309141004_seed_workshopuser.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hechinger.FSK.Infrastructure.Migrations
+{
+    public partial class seed_workshopuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var k = 0;
+            for (int i = 1; i < 8; i++)
+            {
+                for (int j = 1; j < 6; j++)
+                {
+                    k++;
+                    migrationBuilder.InsertData(
+                      table: "WorkshopUsers",
+                      columns: new[] { "Id", "Created", "Creator", "LastModified", "LastModifier", "EntityStatus", "WorkshopId", "UserId" },
+                      values: new object[,]
+                      {
+
+                            { k, DateTime.Now, "SYSTEM",DateTime.Now, "SYSTEM", (int)EntityStatuses.Active,j, i}
+                      });
+
+                }
+               
+            }
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
