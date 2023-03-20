cfd50aab3025b633f8e3a31759fc430fd57dfc28
holeczpeter <holecz.peter85@gmail.com>
2023-03-20 23:37:40 +0100

    Fix all css

--- Hechinger.FSK.Web/FSKClient/src/app/modules/summary-card/summary-card-print/summary-card-print-view/summary-card-print-view.component.ts
+++ Hechinger.FSK.Web/FSKClient/src/app/modules/summary-card/summary-card-print/summary-card-print-view/summary-card-print-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
-import { OperationPrintModel, SelectModel } from '../../../../models/generated/generated';
+import { DefectPrintModel, OperationPrintModel, SelectModel } from '../../../../models/generated/generated';
 import { OperationDataService } from '../../../../services/data/operation-data.service';
 import { LanguageService } from '../../../../services/language/language.service';
 
@@ -16,6 +16,8 @@ export class SummaryCardPrintViewComponent implements OnInit, OnChanges {
   numbers: Array<number>;
   count: number;
   currentDate= new Date();
+  firstList: DefectPrintModel[];
+    secondList: DefectPrintModel[];
   constructor(private readonly operationDataService: OperationDataService,
     public languageService: LanguageService) { }
     
@@ -30,7 +32,11 @@ export class SummaryCardPrintViewComponent implements OnInit, OnChanges {
         this.printableOperation = result;
         if (this.printableOperation.defects.length <= 10)  this.count = 4;
         if (this.printableOperation.defects.length > 10 && this.printableOperation.defects.length <= 33) this.count = 2;
-        if (this.printableOperation.defects.length > 33) this.count = 1;
+        if (this.printableOperation.defects.length > 33) {
+          this.count = 1;
+          this.firstList = this.printableOperation.defects.slice(0, 33);
+          this.secondList = this.printableOperation.defects.slice(33, this.printableOperation.defects.length-1);
+        } 
         this.numbers = Array(this.count).fill(0).map((x, i) => i);
         console.log(this.numbers);
         console.log(this.count)
