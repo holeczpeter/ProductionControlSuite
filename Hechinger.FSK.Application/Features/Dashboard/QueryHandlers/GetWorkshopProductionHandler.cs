f99aa950c93f196534ba02176972fc67b7f6b0ea
holeczpeter <holecz.peter85@gmail.com>
2023-04-13 19:59:35 +0200

    Fix workshopproduction

--- Hechinger.FSK.Application/Features/Dashboard/QueryHandlers/GetWorkshopProductionHandler.cs
+++ Hechinger.FSK.Application/Features/Dashboard/QueryHandlers/GetWorkshopProductionHandler.cs
@@ -1,26 +1,21 @@
-﻿using System.Diagnostics;
-
-namespace Hechinger.FSK.Application.Features
+﻿namespace Hechinger.FSK.Application.Features
 {
     public class GetWorkshopProductionHandler : IRequestHandler<GetWorkshopProduction, IEnumerable<WorkshopProduction>>
     {
         private readonly FSKDbContext context;
-        private readonly IQualityService qualityService;
-        public GetWorkshopProductionHandler(FSKDbContext context, IQualityService qualityService)
+        public GetWorkshopProductionHandler(FSKDbContext context)
         {
             this.context = context ?? throw new ArgumentNullException(nameof(context));
-            this.qualityService = qualityService ?? throw new ArgumentNullException(nameof(qualityService));
         }
-
 
         public async Task<IEnumerable<WorkshopProduction>> Handle(GetWorkshopProduction request, CancellationToken cancellationToken)
         {
-            if(cancellationToken.IsCancellationRequested) { Debug.WriteLine("CancellationToken"); }
+            
             var workshops = await this.context.Workshops
                 .AsNoTracking()
                 .Select(w => new { Id = w.Id, Name = w.Name })
                 .ToListAsync(cancellationToken);
-            if (cancellationToken.IsCancellationRequested) { return null; }
+
             var cardItems = await this.context.SummaryCards
                  .Where(sc => sc.Date.Date >= request.StartDate.Date.Date &&
                               sc.Date.Date <= request.EndDate.Date &&
@@ -32,7 +27,7 @@ namespace Hechinger.FSK.Application.Features
                      Quantity = sc.Quantity,
                      DefectQuantity = sc.DefectQuantity
                  }).ToListAsync(cancellationToken);
-            if (cancellationToken.IsCancellationRequested) { Debug.WriteLine("CancellationToken"); }
+           
 
             var items = cardItems.GroupBy(sc => new { WorkshopId = sc.WorkshopId, Date = sc.Date.Date }).Select(g => new ProductionDayInfo()
             {
@@ -41,17 +36,15 @@ namespace Hechinger.FSK.Application.Features
                 DefectQuantity = g.ToList().Select(x => x.DefectQuantity).Sum(),
                 Quantity = g.ToList().Select(x => x.Quantity).Sum(),
             }).ToList();
-            if (cancellationToken.IsCancellationRequested) { Debug.WriteLine("CancellationToken"); }
-            var results = workshops.Select(w => new WorkshopProduction()
+
+            return workshops.Select(w => new WorkshopProduction()
             {
                 WorkshopId = w.Id,
                 WorkshopName = w.Name,
                 Days = items.Where(x => x.WorkshopId == w.Id).ToList(),
 
             }).ToList();
-            if (cancellationToken.IsCancellationRequested) { Debug.WriteLine("CancellationToken"); }
-            return results;
-
+            
         }
     }
 }
